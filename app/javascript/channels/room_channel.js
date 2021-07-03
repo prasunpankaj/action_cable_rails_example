92c922d8141bd95161b07c6ab756dd574eeeff94
Pankaj Prasun <you@example.com>
2021-07-03 17:20:55 +0530

    Action Cabel

---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,29 @@
+import consumer from "./consumer"
+
+document.addEventListener('turbolinks:load', () =>{
+
+const element = document.getElementById('room-id');
+const room_id = element.getAttribute("data-room-id");
+
+consumer.subscriptions.create({ channel: "RoomChannel",
+  room_id: room_id }, {
+  connected() {
+    //alert("Connected to room channel....")
+    console.log("Connected to room channel " + room_id);
+    //window.open("https://www.google.com");
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+     console.log("Disconnected to room channel....");
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+     //console.log("Received Data room channel....")
+     console.log(data);
+     //alert("Connected to room channel....")
+    // Called when there's incoming data on the websocket for this channel
+  }
+});
+})
