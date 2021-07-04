c25ce933ed93455739a4b026bee39fc4d2ad5756
Pankaj Prasun <you@example.com>
2021-07-04 16:06:26 +0530

    Comment Article

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,6 @@ class CommentsController < ApplicationController
   http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
 
 	def create
-    binding.pry
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
